99a6d44c0e3021de6412a7f54e059edd74eb4c6c
Erik Sanchez Ambriz <eaambriz@amazon.com>
2022-05-06 15:10:36 -0700

    Amend [BBSP-5][BBSP-108] Slight change to VerifyRegistration code.

--- Scripts/VerifyRegistration.js
+++ Scripts/VerifyRegistration.js
@@ -11,7 +11,7 @@ loginSubmit.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
 
-    if(username == "user" && password == "pass"){
+    if(username == "newUser" && password == "newPass"){
         // routes to homepage currently it needs to be updated to proper route when ready
         location.replace("../Pages/Homepage.html");
     }
@@ -19,8 +19,22 @@ loginSubmit.addEventListener("click", (e) => {
         loginForm.reset();
         alert("Missing Username or Password. Please Try again.");
     }
+    else if(username == "user"){
+        loginForm.reset();
+        alert("Username already exists in database. Please Try again.");
+    }
+    else if(username.length > 16){
+        // max length can be changed later
+        loginForm.reset();
+        alert("Maximum size of user name is 16 characters. Please Try again.");
+    }
+    else if(password.length < 8){
+        // max length can be changed later
+        loginForm.reset();
+        alert("Minimum size of password is 8 characters. Please Try again.");
+    }
     else{
         loginForm.reset();
-        alert("Invalid Login. Please try again.");
+        alert("Invalid new username or password. Please try again.");
     }
 });
