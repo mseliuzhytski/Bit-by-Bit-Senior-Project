1cd56884dec40a50ef12d1b3df27e86bacd55408
MelvinE1999 <73800751+MelvinE1999@users.noreply.github.com>
2022-10-08 13:53:08 -0700

    BBSP-164

--- APIs/Inventory/app.js
+++ APIs/Inventory/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
