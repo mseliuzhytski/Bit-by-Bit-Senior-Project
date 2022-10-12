453c56c1340bdac3859376327ad1fdda1051db45
Erik Sanchez Ambriz <eaambriz@amazon.com>
2022-10-12 09:07:33 -0700

    Heroku Backend Init - Fix Submit Form bug.

--- Backend/Routes/aboutUs.js
+++ Backend/Routes/aboutUs.js
@@ -1,48 +1,5 @@
-const alert = require('alert');
 const path = require('path');
-const nodemailer = require('nodemailer');
 
 exports.get = function(req,res){
         res.sendFile(path.join(__dirname+'../../../Pages/Aboutus.html'));
     };
-
-exports.post('/submitForm', (req, res)=> {
-    try {
-        const firstName = req.body.firstName;
-        const lastName = req.body.lastname;
-        const contactPreference = req.body.contactPreference;
-        const contactInfo = req.body.contactInfo;
-        const subject = req.body.subject;
-
-        var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'youremail@gmail.com',
-            pass: 'yourpassword'
-        }
-        });
-
-        var mailOptions = {
-        from: 'youremail@gmail.com',
-        to: 'youremail@gmail.com',
-        subject: 'OKAIDI CONTACT FORM',
-        text: 'Name: ' + firstName + ' ' + lastName + ' \n' + 
-                'Contact preference: ' + ' ' + contactPreference + ' \n' + 
-                'Contact info: ' + ' ' + contactInfo + ' \n\n' + 
-                'Subject: ' + ' ' + subject
-        };
-
-        transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            res.status(500).send('Unable to send contact form.')
-            alert("Unable to send contact form.")
-        } else {
-            res.status(200).send('Email sent: ' + info.response)
-            alert("Contact form sent successfully.")
-        }
-        }); 
-
-    } catch {
-        res.sendFile(path.join(__dirname+'../../../Pages/ContactUs.html'))
-    }
-});
