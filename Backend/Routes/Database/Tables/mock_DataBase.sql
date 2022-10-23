2042f214c12e5974cb7125bba6167388bb5f6ea7
haroon-csc <80446800+haroon-csc@users.noreply.github.com>
2022-10-23 12:45:11 -0700

    BBSP-219 Mock DataBase

---
+++ Backend/Routes/Database/Tables/mock_DataBase.sql
@@ -0,0 +1,42 @@
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Ford', 'Focus', '2015', '$18000', '50000', 'Sedan', 'Used', 'Gray');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Honda', 'Civic', '2018', '$20000', '40000', 'Coupe', 'Used', 'White');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Toyota', 'Tacoma', '2020', '$45000', '25000', 'Truck', 'Used', 'Black');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Toyota', 'Carolla', '2022', '$29000', '0', 'Sedan', 'New', 'Red');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M3', 'Competition xDrive', '2022', '$85000', '70', 'Sedan', 'New', 'Sao Paulo Yellow');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M5 CS', '2023', '$149899', '30', 'Sedan', 'New', 'Blue');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Mercedes-Benz', 'AMG G63 4MATIC', '2022', '$220000', '80', 'suv', 'New', 'MANUFAKTUR Platinum Magno (matte)');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Mercedes-Benz', 'E53', '2022', '$80000', '20', 'Sedan', 'New', 'Nautical Blue metallic');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Maserati', 'Ghibli Modena Q4', '2023', '$95000', '10', 'Sedan', 'New', 'Blu Emozione Metallic
+');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Dodge', 'Challenger SRT Demon', '2018', '$151000', '6000', 'Sedan', 'Used', '
+Destroyer Gray Clearcoat');
+
+
+
+INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
+VALUES ('Mohammed', 'mohammed@okaidiautosales.com', '9161112213', 'General Manager');
+
+INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
+VALUES ('Chris', 'chris@okaidiautosales.com', '9161112211', 'Sales Preson');
+
+INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
+VALUES ('John', 'john@okaidiautosales.com', '9161112212', 'Finance Manager');
