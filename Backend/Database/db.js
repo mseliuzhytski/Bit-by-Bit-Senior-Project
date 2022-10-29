82aa1e2bda637e2ade48d70353cd86a3e7aaba73
Justin Chau <justjustinchau@gmail.com>
2022-10-28 18:18:27 -0700

    fix db

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
-    user: 'bab5e4a2834513',
-    password: '1b5f71bd',
-    database: 'heroku_9b0d7a28a628498'
+    user: 'b49a1b4f62d440',
+    password: '105c9183',
+    database: 'heroku_b86450961ab53ab'
 });
 
 module.exports = db;
