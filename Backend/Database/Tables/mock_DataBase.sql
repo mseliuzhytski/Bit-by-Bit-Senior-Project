1980d05773c0f9192f73354c14f3c5ad62a5f2a2
Erik Sanchez Ambriz <eaambriz@amazon.com>
2022-10-29 04:58:20 -0700

    BBSP-231 Standardize tables to meet 2NF

--- Backend/Database/Tables/mock_DataBase.sql
+++ Backend/Database/Tables/mock_DataBase.sql
@@ -1,42 +1,128 @@
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Ford', 'Focus', '2015', '$18000', '50000', 'Sedan', 'Used', 'Gray');
+VALUES ('Ford', 'Focus', '2015', '18000', '16000', 'Sedan', 'Used', 'Gray');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Honda', 'Civic', '2018', '$20000', '40000', 'Coupe', 'Used', 'White');
+VALUES ('Ford', 'F-150', '2023', '70000', '20', 'Truck', 'New', 'Silver');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Toyota', 'Tacoma', '2020', '$45000', '25000', 'Truck', 'Used', 'Black');
+VALUES ('Ford', 'F-250', '2023', '90000', '25', 'Truck', 'New', 'Black');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Toyota', 'Carolla', '2022', '$29000', '0', 'Sedan', 'New', 'Red');
+VALUES ('Honda', 'Civic', '2018', '20000', '25000', 'Coupe', 'Used', 'White');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('BMW', 'M3', 'Competition xDrive', '2022', '$85000', '70', 'Sedan', 'New', 'Sao Paulo Yellow');
+VALUES ('Honda', 'Civic', '2019', '28000', '21000', 'Sedan', 'Used', 'Red');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('BMW', 'M5 CS', '2023', '$149899', '30', 'Sedan', 'New', 'Blue');
+VALUES ('Honda', 'CR-V', '2014', '12000', '15000', 'SUV', 'Used', 'Gray');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Mercedes-Benz', 'AMG G63 4MATIC', '2022', '$220000', '80', 'suv', 'New', 'MANUFAKTUR Platinum Magno (matte)');
+VALUES ('Honda', 'Odyssey', '2023', '48500', '0', 'Van', 'New', 'White');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Mercedes-Benz', 'E53', '2022', '$80000', '20', 'Sedan', 'New', 'Nautical Blue metallic');
+VALUES ('Nissan', 'NV200', '2020', '19000', '15000', 'Van', 'Used', 'Silver');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Maserati', 'Ghibli Modena Q4', '2023', '$95000', '10', 'Sedan', 'New', 'Blu Emozione Metallic
-');
+VALUES ('Nissan', 'Altima', '2012', '14300', '105000', 'Coupe', 'Used', 'Black');
 
 INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
-VALUES ('Dodge', 'Challenger SRT Demon', '2018', '$151000', '6000', 'Sedan', 'Used', '
-Destroyer Gray Clearcoat');
+VALUES ('Toyota', 'Tacoma', '2020', '45000', '25000', 'Truck', 'Used', 'Black');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Toyota', 'Carolla', '2022', '29000', '10', 'Sedan', 'New', 'Red');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Toyota', 'Carolla', '2021', '24000', '12000', 'Sedan', 'Used', 'Red');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Toyota', 'Carolla', '2021', '25000', '1300', 'Sedan', 'Used', 'White');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M3 Competition xDrive', '2022', '85000', '70', 'Sedan', 'New', 'Sao Paulo Yellow');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M5 CS', '2023', '149899', '30', 'Sedan', 'New', 'Blue');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M5 CS', '2023', '149899', '25', 'Sedan', 'New', 'Blue');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('BMW', 'M5 CS', '2023', '155300', '20', 'Sedan', 'New', 'Black');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Mercedes-Benz', 'AMG G63 4MATIC', '2022', '220000', '80', 'SUV', 'New', 'MANUFAKTUR Platinum Magno (matte)');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Mercedes-Benz', 'E53', '2022', '80000', '20', 'Sedan', 'New', 'Nautical Blue Metallic');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Mercedes-Benz', 'E53', '2022', '80000', '50', 'Sedan', 'New', 'Nautical Blue Metallic');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Maserati', 'Ghibli Modena Q4', '2023', '95000', '10', 'Sedan', 'New', 'Blu Emozione Metallic');
+
+INSERT INTO INVENTORY (Car_Make, Car_Model, Car_Year, Car_Price, Car_Mileage, Car_BodyType, Car_Condition, Car_Color)
+VALUES ('Dodge', 'Challenger SRT Demon', '2018', '151000', '6000', 'Sedan', 'Used', 'Destroyer Gray Clearcoat');
 
 
 
-INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
-VALUES ('Mohammed', 'mohammed@okaidiautosales.com', '9161112213', 'General Manager');
+INSERT INTO EMPLOYEE (Emp_Username, Emp_PasswordHash, Emp_FirstName, Emp_LastName, Emp_Role)
+VALUES ('moham4321', 'e66a124f9cabd3198d84dd68c8c87cf7', 'Mohammed', 'Smith', 'General Manager');
 
-INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
-VALUES ('Chris', 'chris@okaidiautosales.com', '9161112211', 'Sales Preson');
+INSERT INTO EMPLOYEE (Emp_Username, Emp_PasswordHash, Emp_FirstName, Emp_LastName, Emp_Role)
+VALUES ('chrisRS1212', '9ed083b1436e5f40ef984b28255eef18', 'Chris', 'Smith', 'Sales Person');
 
-INSERT INTO EMPLOYEE (Emp_Name, Emp_email, Emp_phone, Emp_Role)
-VALUES ('John', 'john@okaidiautosales.com', '9161112212', 'Finance Manager');
+INSERT INTO EMPLOYEE (Emp_Username, Emp_PasswordHash, Emp_FirstName, Emp_LastName, Emp_Role)
+VALUES ('johnGreen01', '57336afd1f4b40dfd9f5731e35302fe5', 'John', 'Green', 'Finance Manager');
+
+INSERT INTO EMPLOYEE (Emp_Username, Emp_PasswordHash, Emp_FirstName, Emp_LastName, Emp_Role)
+VALUES ('erik123', '230d3b2452c6b7e32b74b2beae943a49', 'Erik', 'Johnson', 'Sales Person');
+
+INSERT INTO EMPLOYEE (Emp_Username, Emp_PasswordHash, Emp_FirstName, Emp_LastName, Emp_Role)
+VALUES ('gummybear916', 'ec9903c79dd510ffa43f69ee867a9002', 'Justin', 'Jones', 'Sales Person');
+
+
+/* Mohammed(4) and Erik(34) get two phone numbers */
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('4', '9165453232');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('4', '9165451111');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('14', '9162335555');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('24', '9167776161');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('34', '8054442222');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('34', '9162662121');
+
+INSERT INTO PHONE (Emp_ID, Emp_phone)
+VALUES ('44', '9165551111');
+
+
+/* Mohammed(4) and Justin(44) get two email addresses */
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('4', 'mohammed@okaidiautosales.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('4', 'mohammed333@gmail.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('14', 'chris@okaidiautosales.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('24', 'john@okaidiautosales.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('34', 'erik@okaidiautosales.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('44', 'justin@okaidiautosales.com');
+
+INSERT INTO EMAIL (Emp_ID, Emp_Email)
+VALUES ('44', 'justin121@gmail.com');
