8bc280456bd905017a17e91e9adda7669e79f583
Erik Sanchez Ambriz <eaambriz@amazon.com>
2022-11-12 03:22:53 -0800

    DB connection limit 7, Landingpage fix - navi .html to .ejs

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -4,7 +4,8 @@ const db = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b49a1b4f62d440',
     password: '105c9183',
-    database: 'heroku_b86450961ab53ab'
+    database: 'heroku_b86450961ab53ab',
+    connectionLimit: 7
 });
 
 module.exports = db;
